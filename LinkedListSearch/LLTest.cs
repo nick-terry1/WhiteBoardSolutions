80cb3188e7cc1beebb34393cc8fb6cf0a222b968
Nick Terry <nick.terry1@outlook.com>
2018-05-21 17:51:03 -0400

    changed the linked list program to a class file, and added a unit test file

---
+++ LinkedListSearch/LLTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedListNode;
+
+namespace LLTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTrueCase()
+        {
+            Node first = new Node(1, null);
+            Node second = new Node(2, first);
+            Node third = new Node(3, second);
+            Node fourth = new Node(4, third);
+
+            Node.Search(fourth, 2);
+            
+            // returns true
+        }
+        [TestMethod]
+        public void TestFalseCase()
+        {
+            Node b1 = new Node(50, null);
+            Node b2 = new Node(25, b1);
+
+            Node.Search(b2, 10);
+
+            // returns false
+        }
+        [TestMethod]
+        public void TestNullHead()
+        {
+            Node nullHead = null;
+
+            Node.Search(nullHead, 4);
+
+            // returns "ERROR: the head cannot be null" \n false
+        }
+    }
+}
---
+++ LinkedListSearch/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// created by Nick Terry
+// last edit: may 21, 2018
+// desc: creates a linked list class with a search method that looks for an integer value
+
+namespace LinkedListNode
+{
+    public class Node
+    {
+        public int value { get; set; }
+        public Node next { get; set; }
+
+        public Node(int value, Node head)
+        {
+            next = head;
+            this.value = value;
+        }
+
+        public static bool Search(Node head, int find)
+        {
+            try
+            {
+                if (head.value == find)
+                {
+                    return true;
+                }
+                if (head.next == null)
+                {
+                    return false;
+                }
+                else return Search(head.next, find);
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("ERROR: the head cannot be null");
+                return false;
+            }
+        }
+    }
+}
